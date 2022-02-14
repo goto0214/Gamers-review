bc921c9ab86f7eba9c390c3ce0cc799b8057a0f7
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-02-14 12:16:15 +0000

    [Update]layouts

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,9 +6,13 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to admin_users_path
   end
 
 
